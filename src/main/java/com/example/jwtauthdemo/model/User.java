effccbcd6814f67fcf97f743fce97e94a10cc378
GabrielPresense <gabrielpresense@gmail.com>
2025-06-12 14:27:15 -0300

    Implementação da User

---
+++ src/main/java/com/example/jwtauthdemo/model/User.java
@@ -0,0 +1,37 @@
+package com.example.jwtauthdemo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+    
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+} 
