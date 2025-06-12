b6e0035c02be4153dec1204a61b6b1a5d3762eb1
GabrielPresense <gabrielpresense@gmail.com>
2025-06-12 12:50:17 -0300

    first commit

---
+++ src/main/java/com/example/projetoJTW/ProjetoJtwApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoJTW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJtwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoJTW/ProjetoJtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoJTW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJtwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
